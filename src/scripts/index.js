09e41d12d9c2ccff4b2b18dd7fb29f20f15e341b
Luca Morandi <luca@digitpaint.nl>
2020-09-15 23:58:23 +0200

    Add swanky nebula background

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
 // import { qs, qsa, $on, $delegate } from './utils';
-
+import { generateNebula } from './nebula';
 import '../stylesheets/style.scss';
+
+generateNebula(document.querySelector('.header'));
---
+++ src/scripts/nebula.js
@@ -0,0 +1,89 @@
+import * as THREE from 'three';
+
+/**
+ * Copyright (c) 2020 by RyanIndustries8 (https://codepen.io/ryanindustries8/pen/XWdYdGz)
+ */
+
+export function generateNebula(node) {
+  let scene, camera, composer, renderer, cloudParticles = [];
+
+  function init(node) {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(35, node.offsetWidth / node.offsetHeight, 1, 1000);
+    camera.position.z = 1;
+    camera.rotation.x = 1.16;
+    camera.rotation.y = -0.12;
+    camera.rotation.z = 0.27;
+
+    let ambient = new THREE.AmbientLight(0x55555);
+    scene.add(ambient);
+
+    let directionLight = new THREE.DirectionalLight(0xff8c19);
+    directionLight.position.set(0,0,1);
+    scene.add(directionLight);
+
+    let orangeLight = new THREE.PointLight(0xcc6600, 50, 450, 1.7);
+    orangeLight.position.set(200,300,100);
+    scene.add(orangeLight);
+
+    let redLight = new THREE.PointLight(0xd8547e, 50, 450, 1.7);
+    redLight.position.set(100,300,100);
+    scene.add(redLight);
+
+    let blueLight = new THREE.PointLight(0x3677ac, 50, 450, 1.7);
+    blueLight.position.set(300,300,200);
+    scene.add(blueLight);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(node.offsetWidth, node.offsetHeight);
+    scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+    renderer.setClearColor(scene.fog.color);
+
+    node.appendChild(renderer.domElement);
+
+    let loader = new THREE.TextureLoader();
+
+    loader.load("./cloud.png", function(texture) {
+      let cloudGeo = new THREE.PlaneBufferGeometry(500,500);
+      let cloudMaterial = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true
+      });
+
+      for(let i = 0; i < 50; i++) {
+        let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+        cloud.position.set(
+          Math.random() * 800 - 400,
+          500,
+          Math.random() * 500 - 500
+        );
+        cloud.rotation.x = 1.16;
+        cloud.rotation.y = -0.12;
+        cloud.rotation.z = Math.random() * 2 * Math.PI;
+        cloud.material.opacity = 0.07;
+        cloudParticles.push(cloud);
+        scene.add(cloud);
+      }
+    });
+
+    window.addEventListener('resize', onWindowResize(node), false);
+    render();
+  }
+
+  function onWindowResize(node) {
+    camera.aspect = node.offsetWidth / node.offsetHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(node.offsetWidth, node.offsetHeight);
+  }
+
+  function render() {
+    cloudParticles.forEach(i => {
+      i.rotation.z -=0.001
+    });
+
+    renderer.render(scene,camera);
+    requestAnimationFrame(render);
+  }
+
+  init(node);
+}
